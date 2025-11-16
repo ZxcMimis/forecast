da6c7c1de560ebbee1a1a74d5bd5cc70138795e8
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-11-16 16:12:58 +0200

    burger-menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,20 @@ import '../reset/reset.scss'
 import { Container } from '../Container/Container';
 import logo from "../../img/logo.png"
 import user from "../../img/user.png"
+import React, { useState } from 'react';
+import { Button, Drawer, theme } from 'antd';
 export const Header = () => {
+
+    const { token } = theme.useToken();
+    const [open, setOpen] = useState(false);
+    const showDrawer = () => {
+        setOpen(true);
+    };
+    const onClose = () => {
+        setOpen(false);
+    };
+
+
     return (
 
         <header class="header">
@@ -25,8 +38,42 @@ export const Header = () => {
                         <button class="header__sign-up-button">Sign Up</button>
                         <img src={user} alt="#" />
                     </div>
+
+
+                    <div className='header__menu-box' >
+                        <div>
+                            <Button type="primary" onClick={showDrawer}>
+                                Open
+                            </Button>
+                        </div>
+                        <Drawer
+                            placement="right"
+                            closable={false}
+                            onClose={onClose}
+                            open={open}
+                            getContainer={false}
+                        >
+                            <ul className='header__menu-list'>
+                                <li className="header__menu-item">
+                                    <a href="#" className="header__menu-link">Who we are</a>
+                                </li>
+                                <li className="header__menu-item">
+                                    <a href="#" className="header__menu-link">Contacts</a>
+                                </li>
+                                <li className="header__menu-item">
+                                    <a href="#" className="header__menu-link">Menu</a>
+                                </li>
+                            </ul>
+
+                            <div class="header__user menu">
+                                <button class="header__sign-up-button menu">Sign Up</button>
+                                <img src={user} alt="#" />
+                            </div>
+                        </Drawer>
+                    </div>
+
                 </div>
-        </Container>
+            </Container>
         </header>
 
     )
